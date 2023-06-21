e9e03db4652132e952765551a9a3d532855f1228
Bob Kozdemba <bkozdemba@gmail.com>
2023-06-20 20:19:28 -0500

    added env debug

--- src/01-yahoo2csv.py
+++ src/01-yahoo2csv.py
@@ -7,12 +7,14 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+import os
 
 if __name__ == '__main__':
     ticker = 'IBM'
     filename = ticker + '.csv'
     yf.pdr_override()
     df = pdr.get_data_yahoo(ticker, start='2023-01-01', end='2023-06-01')
+    print(f'******* Env BOB = {os.getenv("BOB")}')
     print(f'Count = {df.count()}')
     print(f'Saving as {filename}')
     df.to_csv(filename)
