190faa937b69466214fc579094aa0df84d531d3f
Cory Latschkowski <koree@redhat.com>
2023-07-17 06:52:30 -0600

    update: make pipeline more interesting

--- src/01-get-data.py
+++ src/01-get-data.py
@@ -3,28 +3,51 @@
 #
 
 import os
+import argparse
 
 import yfinance as yf
 from pandas_datareader import data as pdr
 
 
+def get_args():
+
+    parser = argparse.ArgumentParser(
+             description="Get Stock Info"
+    )
+
+    # parser.set_defaults("IBM")
+    parser.add_argument("ticker", nargs="?", type=str, help="Stock Ticker", default="IBM")
+    parser.add_argument("-s", "--start-date", type=str, help="Start Date", default="2023-01-01")
+    parser.add_argument("-e", "--end-date", type=str, help="End Date", default="2023-06-01")
+
+    args = parser.parse_args()
+
+    try:
+        return args
+    except:
+        print(err, args)
+
 def get_stock_data(ticker, start_date, end_date, filename):
+
+    print(f"Ticker: {ticker}")
 
     yf.pdr_override()
     df = pdr.get_data_yahoo(ticker, start=start_date, end=end_date)
 
-    print(f"Count = {df.count()}")
-    print(f"Saving as {filename}")
+    print(f"Count: \n{df.count()}")
+    print(f"Saving: {filename}...")
 
     df.to_csv(filename)
 
 
 if __name__ == "__main__":
 
+    args = get_args()
+
     scratch = "../scratch/"
     os.makedirs(scratch, exist_ok=True)
 
-    ticker = os.getenv("TICKER", "IBM")
-    filename = scratch + ticker + "-sample.csv"
+    # ticker = os.getenv("TICKER", "IBM")
+    filename = scratch + args.ticker.upper() + "-sample.csv"
     
-    get_stock_data(ticker, start_date="2023-01-01", end_date="2023-06-01", filename=filename)
+    get_stock_data(args.ticker, start_date=args.start_date, end_date=args.end_date, filename=filename)
